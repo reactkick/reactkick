45a546491d67df46ecc0536e52a4747f0b2a179a
@reactkick <kad.rselcuk@gmail.com>
2025-06-24 23:41:44 +0300

    Create uygulama.tsx

---
+++ uygulama.tsx
@@ -0,0 +1,29 @@
+import { StackHandler, StackProvider, StackTheme } from "@stackframe/react";
+import { Suspense } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { stackClientApp } from "./stack";
+
+function HandlerRoutes() {
+  const location = useLocation();
+  
+  return (
+    <StackHandler app={stackClientApp} location={location.pathname} fullPage />
+  );
+}
+
+export default function App() {
+  return (
+    <Suspense fallback={null}>
+      <BrowserRouter>
+        <StackProvider app={stackClientApp}>
+          <StackTheme>
+            <Routes>
+              <Route path="/handler/*" element={<HandlerRoutes />} />
+              <Route path="/" element={<div>hello world</div>} />
+            </Routes>
+          </StackTheme>
+        </StackProvider>
+      </BrowserRouter>
+    </Suspense>
+  );
+}
