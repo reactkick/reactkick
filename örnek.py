342e0e541450c51c2bcf90aacd406a70afaa66f8
@reactkick <kad.rselcuk@gmail.com>
2025-06-27 01:12:26 +0300

    Delete örnek.py

--- örnek.py
+++
@@ -1,7 +0,0 @@
-# Get current project info
-print(stack_auth_request('GET', '/api/v1/projects/current'))
-
-# Get user info with access token
-print(stack_auth_request('GET', '/api/v1/users/me', headers={
-  'x-stack-access-token': access_token,
-}))
